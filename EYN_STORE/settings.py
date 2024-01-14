cf88a188be8c043a7f82136b4564fb2d9d86c821
Shyam2615 <syb26633@gmail.com>
2024-01-14 14:12:31 +0530

    z

--- EYN_STORE/settings.py
+++ EYN_STORE/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -143,7 +142,6 @@ BASE_DIR
 
 
 MEDIA_URL = '/media/'
-django_heroku.settings(locals())
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
