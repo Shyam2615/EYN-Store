e8f071cfe7f13136ab7f0206273c6edc64b65122
Shyam2615 <syb26633@gmail.com>
2024-02-02 12:25:03 +0530

    url changed

--- EYN_STORE/urls.py
+++ EYN_STORE/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('delete-wishlist/<product_uuid>/', delete_wishlist, name="deleteWishlist"),
 ]
 
-if settings.DEBUG: 
-    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
