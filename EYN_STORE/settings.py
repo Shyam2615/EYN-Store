34d334653bbe61718084467a7b4701985deb6b00
Shyam2615 <syb26633@gmail.com>
2024-02-02 12:13:12 +0530

    Static changed

--- EYN_STORE/settings.py
+++ EYN_STORE/settings.py
@@ -125,12 +125,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -150,6 +144,8 @@ BASE_DIR
 
 
 MEDIA_URL = '/media/'
+
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = 'home.backends.email_backend.EmailBackend'
