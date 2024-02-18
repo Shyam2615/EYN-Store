08058c516355652737243caf60ae820c44432533
Shyam2615 <syb26633@gmail.com>
2024-02-18 14:47:51 +0530

    Static roots changed

--- EYN_STORE/settings.py
+++ EYN_STORE/settings.py
@@ -130,22 +130,18 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
-MEDIA_ROOT = (
-BASE_DIR
-)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
-MEDIA_URL = '/media/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
